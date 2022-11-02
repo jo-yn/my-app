d467155c867c9e130d9608afcff5f616e80dc6a7
joyn <honey_nong@naver.com>
2022-11-02 18:02:27 +0900

    리액트입문

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+// 함수형 컴포넌트
+import React from "react";
+
+const Home = () => {
+    return <h1>Home 화면 입니다.</h1>
+}
+
+export default Home;
+//Class 컴포넌트 형
+// import React, {Component} from "react";
+
+// class Home extends Component {
+//     render() {
+//         return <h1>Home 화면 입니다.</h1>
+//     }
+// }
+
+// export default Home;
